e299881b46eb0d5dc6bf3160ab3761b398549fed
dinhco01 <dinhco01@users.noreply.github.com>
2021-10-24 14:04:55 +0700

    Update docstring

--- BatDongSan/BatDongSan/spiders/crawl_data.py
+++ BatDongSan/BatDongSan/spiders/crawl_data.py
@@ -16,6 +16,16 @@ class RealEstateChoTotSpider(Spider):
     api = 'https://gateway.chotot.com/v1/public/ad-listing/{}'
     current_type_name = None
 
+    '''
+    - Phương thức này sẽ lấy dữ liệu 10 trang của từng địa điểm tương ứng với từng loại bất động sản
+        locations: Một dict lưu trữ các địa điểm cần crawl (3 địa điểm)
+        type_real_estates: Loại bất động sản (3 loại bất động sản)
+    - Mỗi loại bất động sản tương ứng với mỗi địa điểm sẽ được lấy dữ liệu 10 trang trên Chotot.com.
+    - Mỗi trang sẽ có một danh sách các sản phẩm, mỗi sản phẩm sẽ có một đường dẫn, trong đây sẽ có một id mà
+      chúng ta sẽ có thể dùng để lấy được các thông tin chi tiết của sản phẩm thông qua việc gọi api.
+    - Mỗi sản phẩm trên từng trang sẽ được gọi api lấy thông tin chi tiết và chuyển đến phương thức parse_item
+      để merge dữ liệu và dataframe
+    '''
     def parse(self, response):
         locations = {"tp-ho-chi-minh": "TP Hồ Chí Minh", "binh-duong": "Bình Dương", "dong-nai": "Đồng Nai"}
         type_real_estates = {"mua-ban-can-ho-chung-cu": "Căn hộ/Chung cư", "mua-ban-nha-dat": "Nhà ở",
@@ -42,6 +52,17 @@ class RealEstateChoTotSpider(Spider):
                     yield scrapy.Request(self.base_url.format(location, type_real_estate, page),
                                          callback=self.parse)
 
+    '''
+    - Phương thức này nhận json từ api đã request trước đó và map vào item
+        response:   là phản hồi sau khi request từ api
+        attribute_map:  là các thuộc tính sẽ được map từ json vào item
+    - Các thuộc tính có type_name là cần mua sẽ bỏ qua.
+    - Loại bất động sản là giá trị hiện tại của current_type_name khi phương thức này được gọi thực hiện.
+    - Trong chuỗi json sẽ có parameter, tương ứng mỗi parameter sẽ có id, value tương ứng (có thể bị khuyết tùy theo dữ liệu), trong đó:
+        id: tương ứng với tên thuộc tính trong attribute_map
+        value: tương ứng với giá trị các thuộc tính đó
+    - Giá trị trả về là item - sẽ được merge vào dataframe sau khi kết thúc quá trình crawl
+    '''
     def parse_item(self, response):
         item = RealEstateItem()
         attribute_map = ['unitnumber', 'ward', 'area', 'region', 'address', 'property_status',
